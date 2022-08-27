a04bfba639a80e6958f3183f9f9fbc7d07827627
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-27 14:32:02 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      Subscribe ZainKeepsCode
+      <Navbar/>
     </div>
   );
 }
