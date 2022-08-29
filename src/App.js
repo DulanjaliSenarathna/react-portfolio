79ec375e386c75b42ca1606c1a11a31963c3aed1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-29 22:54:55 +0530

    Testimonials section done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Services from "./Components/Services/Services";
 import Experience from "./Components/Experience/Experience";
 import Works from "./Components/Works/Works";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Experience/>
       <Works/>
       <Portfolio/>
+      <Testimonials/>
     </div>
   );
 }
