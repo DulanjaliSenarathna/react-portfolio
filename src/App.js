5753f72e119079175d387265cfb94b2ed4785840
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-29 08:39:59 +0530

    Experience done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import './App.css'
 import Intro from "./Components/Intro/Intro";
 import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Intro/>
       <Services/>
+      <Experience/>
     </div>
   );
 }
---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Experience.css'
+
+const Experience = () => {
+  return (
+    <div className="experience">
+        <div className="achievement">
+            <div className="circle">2+</div>
+            <span>Years </span>
+            <span>Experience</span>
+        </div>
+        <div className="achievement">
+        <div className="circle">50+</div>
+            <span>Completed </span>
+            <span>Projects</span>
+        </div>
+        <div className="achievement">
+        <div className="circle">2+</div>
+        <span>Companies</span>
+            <span>Work</span>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
